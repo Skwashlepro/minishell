cb0a48965594e9f46e26702ba13340106fc4cdd7
luctan <luctan@student.42.fr>
2024-08-01 14:37:05 +0200

    all

--- srcs/main.c
+++ srcs/main.c
@@ -6,21 +6,44 @@
 /*   By: luctan <luctan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/30 18:23:02 by luctan            #+#    #+#             */
-/*   Updated: 2024/07/30 18:59:54 by luctan           ###   ########.fr       */
+/*   Updated: 2024/08/01 14:36:57 by luctan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void	INThandler(int sig)
+{
+	signal(sig, SIG_IGN);
+}
+
+char	*prompter(void)
+{
+	char *input;
+
+	input = NULL;
+	signal(SIGINT, INThandler);
+	while (1)
+	{
+		input = readline("$minishell");
+		if (input)
+			break ;
+	}
+	return (input);
+}
+
 int	parsing(char *prompt)
 {
-	
+	if (prompt)
+		return (1);
+	return (0);
 }
 
 int	main()
 {
 	char *prompt;
-	prompt = readline("$minishell:  ");
+
+	prompt = prompter();
 	if (parsing(prompt))
-		printf("sjdfklasjfklsaf");	
+		printf("%s\n", prompt);	
 }
