ad1317e059c5e80571aae7369fcdd7f37e391ea6
Thomas Steve Passin <tpassin@bess-f3r5s2.clusters.42paris.fr>
2024-08-06 17:22:40 +0200

    chek input

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,41 @@
 /*   By: tpassin <tpassin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/30 18:23:02 by luctan            #+#    #+#             */
-/*   Updated: 2024/08/05 21:05:54 by tpassin          ###   ########.fr       */
+/*   Updated: 2024/08/06 17:19:09 by tpassin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+
+char	check_quotes(char *str)
+{
+	char	quote;
+	
+	quote = 'N';
+	while (*str)
+	{
+		if (*str == '\'' && quote == 'N')
+			quote = '\'';
+		else if (*str == '\"' && quote == 'N')
+			quote = '\"';
+		else if (*str == '\'' && quote == '\'')
+			quote = 'N';
+		else if (*str == '\"' && quote == '\"')
+			quote = 'N';
+		str++;
+		
+	}
+	return (quote);
+}
+
+int	check_input(char *str)
+{
+	if (check_quotes(str) != 'N')
+		return (printf("Error\n"), 1);
+	return (0);
+}
+
 
 void	INThandler(int sig)
 {
@@ -24,12 +54,7 @@ char	*prompter(void)
 
 	input = NULL;
 	signal(SIGINT, INThandler);
-	while (1)
-	{
-		input = readline("$minishell ");
-		if (input)
-			break ;
-	}
+	input = readline("$minishell ");
 	return (input);
 }
 
@@ -43,8 +68,16 @@ int	parsing(char *prompt)
 int	main()
 {
 	char *prompt;
-
-	prompt = prompter();
-	if (parsing(prompt))
-		printf("%s\n", prompt);	
+	
+	while (1)
+	{
+		prompt = prompter();
+		printf("%s\n", prompt);
+		if (ft_strncmp(prompt, "exit", 4) == 0)
+			break ;
+		check_input(prompt);
+		free(prompt);
+	}
+	free(prompt);
+	return (0);	
 }
